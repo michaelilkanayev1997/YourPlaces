35f061adf0a2eece5dbfa1d48a035933676c15f8
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-08-27 00:40:54 +0300

    Adding Place-Specific Routes

--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -2,9 +2,43 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET Request in places");
-  res.json({ message: "It works" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "one of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://www.esbnyc.com/sites/default/files/2020-01/ESB%20Day.jpg",
+    address: "20 W 34th St., New York, NY 10001",
+    location: {
+      lat: 40.7485452,
+      lng: -73.9831886,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emp. State Building",
+    description: "one of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://www.esbnyc.com/sites/default/files/2020-01/ESB%20Day.jpg",
+    address: "20 W 34th St., New York, NY 10001",
+    location: {
+      lat: 40.7485734,
+      lng: -73.9852849,
+    },
+    creator: "u2",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid; // {pid:'p1'}
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  res.json({ place }); // => {place} => { place: place }
 });
 
 module.exports = router;
