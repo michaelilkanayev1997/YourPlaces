95aafa1316a455f46b16ccd411ce53ac658dee0c
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-08-27 18:24:38 +0300

    Adding Controllers

---
+++ server/controllers/places-controller.js
@@ -0,0 +1,63 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "one of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://www.esbnyc.com/sites/default/files/2020-01/ESB%20Day.jpg",
+    address: "20 W 34th St., New York, NY 10001",
+    location: {
+      lat: 40.7485452,
+      lng: -73.9831886,
+    },
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Emp. State Building",
+    description: "one of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://www.esbnyc.com/sites/default/files/2020-01/ESB%20Day.jpg",
+    address: "20 W 34th St., New York, NY 10001",
+    location: {
+      lat: 40.7485734,
+      lng: -73.9852849,
+    },
+    creator: "u2",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid; // {pid:'p1'}
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404);
+  }
+
+  res.json({ place }); // => {place} => { place: place }
+};
+
+const getPlaceByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    );
+  }
+
+  res.json({ place }); // => {place} => { place: place }
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUserId = getPlaceByUserId;
--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -1,65 +1,10 @@
 const express = require("express");
 
-const HttpError = require("../models/http-error");
+const placesControllers = require("../controllers/places-controller");
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "one of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://www.esbnyc.com/sites/default/files/2020-01/ESB%20Day.jpg",
-    address: "20 W 34th St., New York, NY 10001",
-    location: {
-      lat: 40.7485452,
-      lng: -73.9831886,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Emp. State Building",
-    description: "one of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://www.esbnyc.com/sites/default/files/2020-01/ESB%20Day.jpg",
-    address: "20 W 34th St., New York, NY 10001",
-    location: {
-      lat: 40.7485734,
-      lng: -73.9852849,
-    },
-    creator: "u2",
-  },
-];
+router.get("/:pid", placesControllers.getPlaceById);
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid; // {pid:'p1'}
-
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
-
-  if (!place) {
-    throw new HttpError("Could not find a place for the provided id.", 404);
-  }
-
-  res.json({ place }); // => {place} => { place: place }
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creator === userId;
-  });
-
-  if (!place) {
-    return next(
-      new HttpError("Could not find a place for the provided user id.", 404)
-    );
-  }
-
-  res.json({ place }); // => {place} => { place: place }
-});
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
 module.exports = router;
