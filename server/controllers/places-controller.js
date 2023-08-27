baea5f49231e311b0cfab77ab4b33cf583952f30
Michael ilkanayev <93651794+michaelilkanayev1997@users.noreply.github.com>
2023-08-27 21:02:03 +0300

    Validating API input (Request Bodies)

--- server/controllers/places-controller.js
+++ server/controllers/places-controller.js
@@ -1,4 +1,5 @@
 const uuid = require("uuid");
+const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 
@@ -62,6 +63,13 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    //There are errors
+    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+  }
+
   const { title, description, coordinates, address, creator } = req.body;
 
   const createdPlace = {
